496d7151649b9e7187f32eeb704fb2f363a30a3e
neumaticc <77523929+neumaticc@users.noreply.github.com>
2021-02-24 10:40:53 -0800

    Update index.js

--- index.js
+++ index.js
@@ -360,7 +360,7 @@ client.on("message", message => {
     if (command === 'socials') {
         const socialEmbed = new MessageEmbed()
             .setTitle(`**Socials**`)
-            .setDescription('[Tiwtter](https://discordjs.guide/)\n[Instagram](https://discordjs.guide/)\n[YouTube](https://discordjs.guide/)')
+            .setDescription('f')
             .setFooter(`z.socials`)
             .setColor(`${embedC}`)
         message.channel.send(socialEmbed);
