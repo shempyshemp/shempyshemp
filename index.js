54b761bad28c687af0e67c19951a7fab73a1042e
neumaticc <77523929+neumaticc@users.noreply.github.com>
2021-02-24 20:15:04 -0800

    Update index.js

--- index.js
+++ index.js
@@ -25,7 +25,7 @@ client.on('guildMemberAdd', member => {
     message.channel.send(welcomeEmbed);
   });
 
-    client.user.setActivity('games with ' + onlineCount + ' people' , { type: 'PLAYING' });
+    client.user.setActivity('Over Prett`s Art Cafe' , { type: 'WATCHING' });
     console.log(`${client.user.username}#${client.user.discriminator} has started, with ${client.users.cache.size} users, in ${client.channels.cache.size} channels of ${client.guilds.cache.size} guilds.`);
 });
 
