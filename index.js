645464a195b1c64cfe0c9e8d92c4c65b1b519d70
neumaticc <77523929+neumaticc@users.noreply.github.com>
2021-02-28 18:43:17 -0800

    Update index.js

--- index.js
+++ index.js
@@ -30,15 +30,14 @@ const activities_list = [
     "${client.guilds.cache.size} members in Prett`s Art Cafe"
     ]; // creates an arraylist containing phrases you want your bot to switch through.
 
-bot.on('ready', () => {
+client.on('ready', () => {
     setInterval(() => {
         const index = Math.floor(Math.random() * (activities_list.length - 1) + 1); // generates a random number between 1 and the length of the activities array list (in this case 5).
-        bot.user.setActivity(activities_list[index] , { type: 'WATCHING' }); // sets bot's activities to one of the phrases in the arraylist.
+        client.user.setActivity(activities_list[index] , { type: 'WATCHING' }); // sets bot's activities to one of the phrases in the arraylist.
     }, 10000); // Runs this every 10 seconds.
 });
 
 client.on("ready", () => {
-    client.user.setActivity('' , { type: 'WATCHING' });
     console.log(`${client.user.username}#${client.user.discriminator} has started, with ${client.users.cache.size} users, in ${client.channels.cache.size} channels of ${client.guilds.cache.size} guilds.`);
 });
 
